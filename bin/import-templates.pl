f1d2afb885bab00f63547248819d41a35b1d3fd5
v-zhuravlev <zhuravlev.vitaly@gmail.com>
2017-11-24 13:28:35 +0300

    changed lang to filter

--- bin/import-templates.pl
+++ bin/import-templates.pl
@@ -14,43 +14,29 @@ use Getopt::Long;
 my $username = 'Admin';
 my $password = 'zabbix';
 my $api_url = 'http://localhost/zabbix/api_jsonrpc.php';
-
-my $zbx;
-my $params;
-my $json;
-my $result;
-my $lang = "EN";
+my $filter = ''; #use this filter to import only files that contain this sequence. XML is hardcoded.
 
 GetOptions(
     "api_url|url=s" =>  \$api_url,
     "password|p=s"       => \$password,
     "username|u=s"      =>   \$username,
-    "lang=s"      =>   \$lang,
+    "filter|lang=s"      =>   \$filter,
    
 ) or die("Error in command line arguments\n");
 
-
-
-$zbx = ZabbixAPI->new( { api_url=>$api_url, username => $username, password => $password } );
+my $zbx = ZabbixAPI->new( { api_url=>$api_url, username => $username, password => $password } );
 
 my $temp = $ARGV[0] or die "Please provide directory with templates as first ARG or the XML file with template\n";
-my @templates;
 if (-d $temp) {
 
     opendir my $dir, $temp or die "Cannot open directory: $temp\n";
     
-    if ($lang eq 'ALL') {
-       @templates = grep { /\.xml$/ && -f "$temp/$_" } readdir($dir);
-    }
-    else {
-       @templates = grep { /_$lang\.xml$/ && -f "$temp/$_" } readdir($dir);
-    }
-    
+    my @templates = grep { /${filter}.*\.xml$/ && -f "$temp/$_" } readdir($dir);
+
     closedir $dir;
 	die "No templates found in directory $temp!\n" if @templates == 0;
 
-
-    $zbx->login();	
+    $zbx->login();
     foreach my $file (sort { $a cmp $b } (@templates)) {
         print "$temp/$file\n";
         $zbx->import_configuration_from_file("$temp/$file");
